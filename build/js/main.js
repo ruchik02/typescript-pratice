e0e08a4db0121d971a836b7ae0b5b6f3bb05ce85
Ruchika Sharma <ruchikasharma@Ruchikas-MacBook-Pro.local>
2023-01-18 16:44:29 +0530

    chapter-1

---
+++ build/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+let firstName = 'ruchika sharma';
+let lastName = "rudra sharma";
+let a = 1;
+let b = 2;
+let d = '16';
+console.log(parseInt(d) / b);
+// it give error The left-hand side of an arithmetic operation must be of type 'any', 'number', 'bigint' or an enum type.
+let c = a + b;
+console.log(c);
+console.log(lastName);
+console.log(firstName);
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+let firstName='ruchika sharma';
+let lastName="rudra sharma";
+let a:number=1;
+let b:number=2;
+let d:string='16';
+console.log(parseInt(d)/b); 
+// it give error The left-hand side of an arithmetic operation must be of type 'any', 'number', 'bigint' or an enum type.
+let c=a+b;
+console.log(c);
+console.log(lastName);
+console.log(firstName);
