7e711f4c36baeba304bc142f9a30e9244233587f
Ruchika Sharma <ruchikasharma@Ruchikas-MacBook-Pro.local>
2023-01-19 12:56:22 +0530

    chapter-3

--- build/js/main.js
+++ build/js/main.js
@@ -1,16 +1,62 @@
 "use strict";
-let myName = 45;
-let myLastName;
-myLastName = '34';
-console.log(myName);
-let c;
-c = 20;
-let d;
-d = true;
-console.log(typeof (myLastName));
-const sum = (a, b) => {
-    return a + b;
+let myObject;
+myObject = {
+    name: "ruchika",
+    age: 22,
 };
-let postId;
-postId = true;
-let id;
+let array;
+array = [1, 2, 3];
+console.log(typeof (array), "8");
+let myArray;
+myArray = [1, 2, 3, 4];
+console.log(myArray[0], "11");
+console.log(myObject, "12");
+let tuple;
+tuple = ["hello", 2, true];
+console.log(tuple[2], "15");
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["yellow"] = 2] = "yellow";
+})(Color || (Color = {}));
+let myColor = Color.Green;
+console.log(myColor, "18");
+let arr1 = ['hey', 'ruchika', 'byee'];
+let arr2 = ['hey', 'byee', 222];
+let arr3 = ['hey', 111, true];
+arr1.push('2');
+console.log(arr1, "24");
+arr2.push('');
+arr2.push(224);
+console.log(arr2, "26");
+let enh = {
+    name: 'ruchika',
+    active: false,
+    albums: [1, "rui"]
+};
+let jp = {
+    name: 'rudra',
+    active: true,
+    albums: [1, 2]
+};
+// jp=enh
+// enh=jp;
+const greetingGuitar = (guitarist) => {
+    if (guitarist.name) {
+        return `hello ${guitarist.name.toUpperCase()}!`;
+    }
+    return `hello!`;
+};
+var Grade;
+(function (Grade) {
+    Grade[Grade["A"] = 2] = "A";
+    Grade[Grade["B"] = 3] = "B";
+    Grade[Grade["D"] = 4] = "D";
+    Grade[Grade["E"] = 5] = "E";
+    Grade[Grade["G"] = 6] = "G";
+    Grade[Grade["Y"] = 7] = "Y";
+})(Grade || (Grade = {}));
+console.log(Grade, "64");
+console.log(Grade.E, "65");
+console.log(greetingGuitar(enh), "66");
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,66 @@
-let myName=45
-let myLastName;
-myLastName='34'
-console.log(myName);
-let c: string | number;
-c=20;
-let d:any
-d=true
-console.log(typeof(myLastName));
-const sum=(a: string,b: number)=>{
-    return a+b;
+let myObject:object;
+myObject={
+    name:"ruchika",
+    age:22,
 }
-let postId: string|boolean;
-postId=true
-let id:RegExp;
+let array:number[];
+array=[1,2,3];
+console.log(typeof(array),"8");
+let myArray:Array<number>;
+myArray=[1,2,3,4];
+console.log(myArray[0],"11");
+console.log(myObject,"12");
+let tuple:[string,number,boolean];
+tuple=["hello",2,true];
+console.log(tuple[2],"15");
+enum Color{Red,Green,yellow}
+let myColor:Color=Color.Green;
+console.log(myColor,"18");
+
+let arr1=['hey','ruchika','byee'];
+let arr2=['hey','byee',222];
+let arr3=['hey',111,true];
+arr1.push('2');
+console.log(arr1,"24");
+arr2.push('');
+arr2.push(224)
+console.log(arr2,"26");
+// type guitar={
+//     name:string,
+//     active:boolean,
+//     albums:(string|number)[]
+// }
+interface guitar{
+    name?:string,
+    active:boolean,
+    albums:(string|number)[]
+}
+let enh:guitar={
+    name:'ruchika',
+    active:false,
+    albums:[1,"rui"]
+}
+let jp:guitar={
+    name:'rudra',
+    active:true,
+    albums:[1,2]
+}
+// jp=enh
+// enh=jp;
+const greetingGuitar=(guitarist:guitar)=>{
+    if(guitarist.name){
+        return `hello ${guitarist.name.toUpperCase()}!`;
+    }
+    return `hello!`
+}
+enum Grade{
+    A=2,
+    B,
+    D,
+    E,
+    G,
+    Y
+}
+console.log(Grade,"64");
+console.log(Grade.E,"65");
+console.log(greetingGuitar(enh),"66");
